c6a04e3f0480bbbdf0e1de83cfbead64f0c96e96
JustinLee27 <justlee@chapman.edu>
2022-06-24 13:43:49 -0700

    parkinfo

--- src/ParkInfo.js
+++ src/ParkInfo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 export function ParkInfo(props) {
-	// description, contact info, entrance fees, directions, operating hours, image
+	// description, contact info, directions, operating hours, image
 	const [error, setError] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(true);
 	const [items, setItems] = useState([]);
@@ -33,6 +33,19 @@ export function ParkInfo(props) {
 			{error && <div>Error: {error.message}</div>}
 			{!isLoaded && <div>Loading...</div>}
 			{items[0] && <div>{items[0].description}</div>}
+			{items[0] && <div>{items[0].contacts.phoneNumbers[0].phoneNumber}</div>}
+			{items[0] && <div>{items[0].contacts.emailAddresses[0].emailAddress}</div>}
+			{items[0] && <div>{items[0].directionsInfo}</div>}
+			{items[0] && <div>{items[0].operatingHours[0].description}</div>}
+			{items[0] && <div>Monday: {items[0].operatingHours[0].standardHours.monday}</div>}
+			{items[0] && <div>Tuesday: {items[0].operatingHours[0].standardHours.tuesday}</div>}
+			{items[0] && <div>Wednesday: {items[0].operatingHours[0].standardHours.wednesday}</div>}
+			{items[0] && <div>Thursday: {items[0].operatingHours[0].standardHours.thursday}</div>}
+			{items[0] && <div>Friday: {items[0].operatingHours[0].standardHours.friday}</div>}
+			{items[0] && <div>Saturday: {items[0].operatingHours[0].standardHours.saturday}</div>}
+			{items[0] && <div>Sunday: {items[0].operatingHours[0].standardHours.sunday}</div>}
+			{items[0] && <div><img src={items[0].images[0].url}></img></div>}
 		</div>
+		
 	);
 }
