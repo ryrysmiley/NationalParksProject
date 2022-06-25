a3d9ca702a35ec7f4b8bd96f74a16e66523aedf0
unknown <rjue@chapman.edu>
2022-06-24 21:59:36 -0700

    default page

--- src/ParkInfo.js
+++ src/ParkInfo.js
@@ -32,46 +32,92 @@ export function ParkInfo(props) {
 		<div class="parkinfo">
 			{error && <div>Error: {error.message}</div>}
 			{!isLoaded && <div>Loading...</div>}
-			{items[0] && <div class="name"><h1>{items[0].fullName}</h1></div>}
-			{items[0] && <div class="description"><strong>Description: </strong>{items[0].description}</div>}
-			{items[0] && <div class="phoneNumber"><strong>Phone Number: </strong>{items[0].contacts.phoneNumbers[0].phoneNumber}</div>}
-			{items[0] && (
-				<div class="email"><strong>Email: </strong>{items[0].contacts.emailAddresses[0].emailAddress}</div>
-			)}
-			{items[0] && <div class="directions"><strong>Directions: </strong>{items[0].directionsInfo}</div>}
-			{items[0] && <div class="operatinghours"><strong>Operating Hours: </strong>{items[0].operatingHours[0].description}</div>}
-			<div class="days">
-			{items[0] && (
-				<div>Monday: {items[0].operatingHours[0].standardHours.monday}</div>
-			)}
-			{items[0] && (
-				<div>Tuesday: {items[0].operatingHours[0].standardHours.tuesday}</div>
-			)}
-			{items[0] && (
-				<div>
-					Wednesday: {items[0].operatingHours[0].standardHours.wednesday}
+			{!items[0] && (
+				<div class="defaultinfo">
+					START <br></br> EXPLORING
 				</div>
 			)}
 			{items[0] && (
-				<div>Thursday: {items[0].operatingHours[0].standardHours.thursday}</div>
+				<div class="name">
+					<h1>{items[0].fullName}</h1>
+				</div>
 			)}
 			{items[0] && (
-				<div>Friday: {items[0].operatingHours[0].standardHours.friday}</div>
+				<div class="description">
+					<strong>Description: </strong>
+					{items[0].description}
+				</div>
 			)}
 			{items[0] && (
-				<div>Saturday: {items[0].operatingHours[0].standardHours.saturday}</div>
+				<div class="phoneNumber">
+					<strong>Phone Number: </strong>
+					{items[0].contacts.phoneNumbers[0].phoneNumber}
+				</div>
 			)}
 			{items[0] && (
-				<div>Sunday: {items[0].operatingHours[0].standardHours.sunday}</div>
+				<div class="email">
+					<strong>Email: </strong>
+					{items[0].contacts.emailAddresses[0].emailAddress}
+				</div>
 			)}
-			</div>
-			{items[0] && <div class="website"><strong>Official Park Website: </strong><a href={items[0].url} target="blank">{items[0].url}</a></div>}
+			{items[0] && (
+				<div class="directions">
+					<strong>Directions: </strong>
+					{items[0].directionsInfo}
+				</div>
+			)}
+			{items[0] && (
+				<div class="operatinghours">
+					<strong>Operating Hours: </strong>
+					{items[0].operatingHours[0].description}
+				</div>
+			)}
+			{items[0] && (
+				<div class="days">
+					{items[0] && (
+						<div>Monday: {items[0].operatingHours[0].standardHours.monday}</div>
+					)}
+					{items[0] && (
+						<div>
+							Tuesday: {items[0].operatingHours[0].standardHours.tuesday}
+						</div>
+					)}
+					{items[0] && (
+						<div>
+							Wednesday: {items[0].operatingHours[0].standardHours.wednesday}
+						</div>
+					)}
+					{items[0] && (
+						<div>
+							Thursday: {items[0].operatingHours[0].standardHours.thursday}
+						</div>
+					)}
+					{items[0] && (
+						<div>Friday: {items[0].operatingHours[0].standardHours.friday}</div>
+					)}
+					{items[0] && (
+						<div>
+							Saturday: {items[0].operatingHours[0].standardHours.saturday}
+						</div>
+					)}
+					{items[0] && (
+						<div>Sunday: {items[0].operatingHours[0].standardHours.sunday}</div>
+					)}
+				</div>
+			)}
+			{items[0] && (
+				<div class="website">
+					<strong>Official Park Website: </strong>
+					<a href={items[0].url} target="blank">
+						{items[0].url}
+					</a>
+				</div>
+			)}
 			{items[0] && (
 				<div class="image">
 					<img src={items[0].images[0].url} alt="balls"></img>
 				</div>
 			)}
-			
 		</div>
 	);
 }
