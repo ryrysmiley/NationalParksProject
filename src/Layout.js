4e9f8514da7a38d6dc200b7bb9aa622398546c0d
ryrysmiley <>
2022-07-06 19:09:50 -1000

    login start and navbar

--- src/Layout.js
+++ src/Layout.js
@@ -1,22 +1,22 @@
 import { Outlet, Link } from "react-router-dom"
 export function Layout(){
     return(
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Explore National Parks</Link>
+        <div class="Navbar">
+            <nav class="Navbar">
+                <ul class="Navbar">
+                    <li class="Navbar">
+                        <Link to="/" class="Navbar">Explore National Parks</Link>
                     </li>
-                    <li>
-                        <Link to="/mysavedparks">Saved Parks</Link>
+                    <li class="Navbar">
+                        <Link to="/mysavedparks" class="Navbar">Saved Parks</Link>
                     </li>
-                    <li>
-                        <Link to="/signin">Sign In</Link>
+                    <li class="Navbar">
+                        <Link to="/signin" class="Navbar">Account</Link>
                     </li>
                 </ul>
             </nav>
 
             <Outlet />
-        </>
+        </div>
     )
 }
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,18 @@
 export function SignIn(){
     return(
-        <div>filler sign in ball</div>
+        <div class="signin">
+            <form>
+                <div>
+                    <h2>SIGN UP</h2>
+                    <label>Email</label><br/>
+                    <input type="email" class="inputboxes" required/><br/>
+                    <label>Password</label><br/>
+                    <input type="text" id="password" class="inputboxes" required/><br/>
+                    <label>Password Confirmation</label><br/>
+                    <input type="text" id="passwordConfirmation" class="inputboxes" required/><br/>
+                    <input type="submit" class="submit"/>
+                </div>
+            </form>
+        </div>
     )
 }
